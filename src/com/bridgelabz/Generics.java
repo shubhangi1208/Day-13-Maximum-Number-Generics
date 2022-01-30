d908f6f2a0c9d2a0aba7426bfb80720b6e6d8092
shubhangi1208 <ashubhangi1996@gmail.com>
2022-01-30 20:18:24 +0530

    ADDED:Generics Class

--- src/com/bridgelabz/Generics.java
+++ src/com/bridgelabz/Generics.java
@@ -1,7 +1,13 @@
 package com.bridgelabz;
 
-public class Generics {
-    public <T extends Comparable<T>> T compareTo(T a,T b,T c){
+public class Generics<T extends Comparable<T>>  {
+    T a,b,c;
+    Generics(T a,T b,T c){
+        this.a=a;
+        this.b=b;
+        this.c=c;
+    }
+    public T compareTo(){
         if (a.compareTo(b)>0){
             if(a.compareTo(c)>0){
                 return a;
@@ -18,13 +24,13 @@ public class Generics {
     }
 
     public static void main(String[] args) {
-        Generics integerComparision=new Generics();
-        System.out.println("Maximum number from given 3 Integer numbers is " +integerComparision.compareTo(10,20,30));
+        Generics integerComparision=new Generics(10,20,30);
+        System.out.println("Maximum number from given 3 Integer numbers is " +integerComparision.compareTo());
 
-        Generics floatComparision=new Generics();
-        System.out.println("Maximum number from given 3 Float numbers is " +floatComparision.compareTo(5.0F,10.5F,2.0F));
+        Generics floatComparision=new Generics(5.0F,10.5F,2.0F);
+        System.out.println("Maximum number from given 3 Float numbers is " +floatComparision.compareTo());
 
-        Generics stringComparision=new Generics();
-        System.out.println("Maximum string from given 3 string is " + stringComparision.compareTo("Apple","Peach","Banana"));
+        Generics stringComparision=new Generics("Apple","Peach","Banana");
+        System.out.println("Maximum string from given 3 string is " + stringComparision.compareTo());
     }
 }
