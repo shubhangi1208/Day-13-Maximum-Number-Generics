9bd9deec5cce955c98271537542ec18383e3ce0c
shubhangi1208 <ashubhangi1996@gmail.com>
2022-01-30 19:20:27 +0530

    ADDED:Find Maximum From Float

--- src/com/bridgelabz/Generics.java
+++ src/com/bridgelabz/Generics.java
@@ -16,8 +16,23 @@ public class Generics {
             }
         }
     }
-
+    public static Float compareTo(Float a,Float b,Float c){
+        if (a>b){
+            if (a>c){
+                return a;
+            }else{
+                return c;
+            }
+        }else{
+            if(b>c){
+                return b;
+            }else{
+                return c;
+            }
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Maximum number from given 3 numbers is " +compareTo(10,20,30));
+        System.out.println("Maximum number from given 3 Integer numbers is " +compareTo(10,20,30));
+        System.out.println("Maximum number from given 3 Float numbers is " +compareTo(5.0F,10.5F,2.0F));
     }
 }
