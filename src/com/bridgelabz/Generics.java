b3b08fa1d42f68ea0d1ebf6bd1203c62ca27c887
shubhangi1208 <ashubhangi1996@gmail.com>
2022-01-30 19:39:55 +0530

    ADDED:Maximum From Strings

--- src/com/bridgelabz/Generics.java
+++ src/com/bridgelabz/Generics.java
@@ -31,8 +31,25 @@ public class Generics {
             }
         }
     }
+    public static String compareTo(String str1,String str2,String str3) {
+        if(str1.compareTo(str2) > 0) {
+            if(str1.compareTo(str3) > 0) {
+                return str1;
+            } else {
+                return str3;
+            }
+        } else {
+            if(str2.compareTo(str3) > 0) {
+                return str2;
+            } else {
+                return str3;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Maximum number from given 3 Integer numbers is " +compareTo(10,20,30));
         System.out.println("Maximum number from given 3 Float numbers is " +compareTo(5.0F,10.5F,2.0F));
+        System.out.println("Maximum string from given 3 string is " + compareTo("Apple","Peach","Banana"));
     }
 }
